568a40fe960bda1964a262b7f952c783cb51798b
Gerhard Lipp <gelipp@gmail.com>
2013-06-05 11:12:19 +0200

    replace callbacks table with more functional step table

--- spec/try_catch_finallly_spec.lua
+++ spec/try_catch_finallly_spec.lua
@@ -2,157 +2,177 @@ local step = require'step'
 
 describe('The step module',function()
     it('is a function (and not a table)',function()
-        assert.is_equal(type(step),'function')
+        assert.is_table(step)
       end)
     
+    local assert_is_step_instance = function(i)
+      assert.is_table(i)
+      assert.is_function(i.success)
+      assert.is_function(i.error)
+      assert.is_table(i.context)
+      assert.is_table(i.result)
+      assert.is_table(i.try)
+    end
+    
     it('try / finally works with no error',function(done)
-        step{
+        step.new{
           try = {
-            async(function(callbacks)
-                assert.is_equal(type(callbacks.success),'function')
-                assert.is_equal(type(callbacks.error),'function')
-                callbacks.success(123,'hello')
+            async(function(step)
+                assert_is_step_instance(step)
+                step.success(123,'hello')
               end),
-            async(function(callbacks,n,s)
+            async(function(step,n,s)
+                assert_is_step_instance(step)
                 assert.is_equal(n,123)
                 assert.is_equal(s,'hello')
-                assert.is_equal(type(callbacks.success),'function')
-                assert.is_equal(type(callbacks.error),'function')
-                callbacks.success('some arg')
+                step.success('some arg')
               end)
           },
-          finally = async(function(s)
+          finally = async(function(step,s)
+              assert_is_step_instance(step)
               assert.is_equal(s,'some arg')
               done()
             end)
-        }
+        }()
       end)
     
     it('tries can be added async',function(done)
-        local try
-        try = {
-          async(function(callbacks)
-              try[2] = async(function(callbacks,n,s)
-                  assert.is_equal(n,123)
-                  assert.is_equal(s,'hello')
-                  assert.is_equal(type(callbacks.success),'function')
-                  assert.is_equal(type(callbacks.error),'function')
-                  callbacks.success('some arg')
-                end)
-              assert.is_equal(type(callbacks.success),'function')
-              assert.is_equal(type(callbacks.error),'function')
-              callbacks.success(123,'hello')
-            end)
-        }
-        step{
-          try = try,
-          finally = async(function(s)
+        step.new{
+          try = {
+            async(function(step)
+                assert.is_equal(#step.try,1)
+                step.try[2] = async(function(step,n,s)
+                    assert.is_equal(#step.try,2)
+                    assert_is_step_instance(step)
+                    assert.is_equal(n,123)
+                    assert.is_equal(s,'hello')
+                    assert.is_equal(type(step.success),'function')
+                    assert.is_equal(type(step.error),'function')
+                    step.success('some arg')
+                  end)
+                assert.is_equal(type(step.success),'function')
+                assert.is_equal(type(step.error),'function')
+                step.success(123,'hello')
+              end)
+          },
+          finally = async(function(step,s)
+              assert.is_equal(#step.result,2)
+              assert.is_same(step.result[1],{123,'hello'})
+              assert.is_same(step.result[2],{'some arg'})
+              assert_is_step_instance(step)
               assert.is_equal(s,'some arg')
               done()
             end)
-        }
+        }()
       end)
     
     it('try / finally works with error',function(done)
         local spies = {
           try = {
-            spy.new(function(callbacks)
-                callbacks.error('terror')
+            spy.new(function(step)
+                step.error('terror')
               end),
             spy.new(function()
               end)
           },
-          catch = spy.new(function(err) end),
+          catch = spy.new(async(function(step,err)
+                assert_is_step_instance(step)
+                assert.is_equal(err,'terror')
+            end)),
         }
-        step{
+        step.new{
           try = spies.try,
           catch = spies.catch,
-          finally = async(function(s)
+          finally = async(function(step,s)
+              assert_is_step_instance(step)
               assert.spy(spies.try[1]).was.called(1)
               assert.spy(spies.try[2]).was_not.called()
-              assert.spy(spies.catch).was.called_with('terror')
               assert.is_nil(s)
               done()
             end)
-        }
+        }()
       end)
     
     it('try / finally works with unexpected error',function(done)
         local foo = {}
         local spies = {
           try = {
-            spy.new(function(callbacks)
+            spy.new(function(step)
                 error(foo)
               end),
             spy.new(function()
               end)
           },
         }
-        step{
+        step.new{
           try = spies.try,
-          catch = async(function(err)
+          catch = async(function(step,err)
+              assert_is_step_instance(step)
               assert.is_same(err,foo)
             end),
-          finally = async(function(s)
+          finally = async(function(step)
+              assert_is_step_instance(step)
               assert.spy(spies.try[1]).was.called(1)
               assert.spy(spies.try[2]).was_not.called()
-              assert.is_nil(s)
               done()
             end)
-        }
+        }()
       end)
     
     
     
     it('try / catch / finally works with no error',function(done)
-        step{
+        step.new{
           try = {
-            async(function(callbacks)
-                assert.is_equal(type(callbacks.success),'function')
-                assert.is_equal(type(callbacks.error),'function')
-                callbacks.success(123,'hello')
+            async(function(step)
+                assert.is_equal(type(step.success),'function')
+                assert.is_equal(type(step.error),'function')
+                step.success(123,'hello')
               end),
-            async(function(callbacks,n,s)
+            async(function(step,n,s)
                 assert.is_equal(n,123)
                 assert.is_equal(s,'hello')
-                assert.is_equal(type(callbacks.success),'function')
-                assert.is_equal(type(callbacks.error),'function')
-                callbacks.success('some arg')
+                assert.is_equal(type(step.success),'function')
+                assert.is_equal(type(step.error),'function')
+                step.success('some arg')
               end)
           },
           catch = async(function(err)
               assert.is_falsy('should not reach here')
             end),
-          finally = async(function(s)
+          finally = async(function(step,s)
+              assert_is_step_instance(step)
               assert.is_equal(s,'some arg')
               done()
             end)
-        }
+        }()
       end)
     
     it('try / catch / finally works with error',function(done)
         local spies = {
           try = {
-            spy.new(function(callbacks)
-                callbacks.error('terror')
+            spy.new(function(step)
+                step.error('terror')
               end),
             spy.new(function()
               end)
           },
-          catch = spy.new(function()
+          catch = async(function(step,err)
+              assert_is_step_instance(step)
+              assert.is_equal(err,'terror')
             end)
         }
-        step{
+        step.new{
           try = spies.try,
           catch = spies.catch,
-          finally = async(function(s)
+          finally = async(function(step,s)
+              assert_is_step_instance(step)
               assert.spy(spies.try[1]).was.called(1)
               assert.spy(spies.try[2]).was_not.called()
-              assert.spy(spies.catch).was.called_with('terror')
               assert.is_nil(s)
               done()
             end)
-        }
+        }()
       end)
     
   end)
--- src/step.lua
+++ src/step.lua
@@ -3,51 +3,67 @@ local tinsert = table.insert
 local try_catch_finally_executer = function(options)
   local try_index = 0
   local tries = options.try
-  local callbacks = {}
-  callbacks.success = function(...)
+  local self = {}
+  local clean = function()
+    setmetatable(self,{})
+    self.result = nil
+    self.context = nil
+    self.try = nil
+    self = nil
+  end
+  self.success = function(...)
+    self.result[try_index] = {...}
     try_index = try_index + 1
     local tryf = tries[try_index]
     if tryf then
-      local args = {callbacks,...}
-      local ok,err = pcall(tryf,callbacks,...)
+      local args = {self,...}
+      local ok,err = pcall(tryf,self,...)
       if not ok then
-        callbacks.error(err)
+        self.error(err)
       end
     else
       if options.finally then
-        local ok,err = pcall(options.finally,...)
+        local ok,err = pcall(options.finally,self,...)
+        clean()
         if not ok then
           print('lua-step finally function failed:',err)
         end
       end
     end
   end
-  callbacks.error = function(...)
+  self.error = function(...)
     if options.catch then
-      local ok,err = pcall(options.catch,...)
+      local ok,err = pcall(options.catch,self,...)
       if not ok then
         print('lua-step catch function failed:',err)
       end
     end
     if options.finally then
-      local ok,err = pcall(options.finally)
+      local ok,err = pcall(options.finally,self)
+      clean()
       if not ok then
         print('lua-step finally function failed:',err)
       end
     end
   end
-  return callbacks.success
+  self.context = {}
+  self.result = {}
+  self.try = tries
+  self = setmetatable(self,{__newindex = function() error('lua-step table is read-only') end})
+  return self.success
 end
 
-local step = function(config)
+local new = function(config)
   local executer
   if config.try and (config.catch or config.finally) then
     executer = try_catch_finally_executer(config)
   end
   if not executer then
-    error('invalid step config')
+    error('lua-step invalid config')
   end
-  executer()
+  return executer
 end
 
-return step
+return {
+  new = new
+}
